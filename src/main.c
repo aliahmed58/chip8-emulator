3097afd13fca258499955471feb1617d06a265c6
aliahmed58 <aliahmednadeem5859@gmail.com>
2023-12-25 03:34:27 +0500

    init memory with default and fonts

--- src/main.c
+++ src/main.c
@@ -1,36 +1,31 @@
-#include <SDL2/SDL.h>
 #include "../include/renderer.h"
+#include <SDL3/SDL.h>
 
-int main(int argc, char *argv[])
-{
-    struct SDL_struct sdl_struct;
-    sdl_struct.sdl_init = sdl_init;
-    sdl_struct.sdl_init(&sdl_struct, 500, 500, "something");
+int main(int argc, char *argv[]) {
+  struct SDL_struct sdl_struct;
+  sdl_struct.sdl_init = sdl_init;
+  sdl_struct.sdl_init(&sdl_struct, 500, 500, "something");
 
-    while (1)
-    {
-        int finished = 0;
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
-                finished = 1;
-                break;
-            }
-        }
-        if (finished)
-        {
-            break;
-        }
-
-        SDL_SetRenderDrawColor(sdl_struct.renderer, 255, 255, 255, 255);
-        SDL_RenderClear(sdl_struct.renderer);
-        SDL_RenderPresent(sdl_struct.renderer);
+  while (1) {
+    int finished = 0;
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        finished = 1;
+        break;
+      }
+    }
+    if (finished) {
+      break;
     }
 
-    SDL_DestroyRenderer(sdl_struct.renderer);
-    SDL_DestroyWindow(sdl_struct.window);
+    SDL_SetRenderDrawColor(sdl_struct.renderer, 255, 255, 255, 255);
+    SDL_RenderClear(sdl_struct.renderer);
+    SDL_RenderPresent(sdl_struct.renderer);
+  }
 
-    SDL_Quit();
+  SDL_DestroyRenderer(sdl_struct.renderer);
+  SDL_DestroyWindow(sdl_struct.window);
+
+  SDL_Quit();
 }
