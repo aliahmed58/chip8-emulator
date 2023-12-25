cafbbd632e9aa24740f71fd11133fff4e4cfdcc4
aliahmed58 <aliahmednadeem5859@gmail.com>
2023-12-26 02:04:36 +0500

    testing with display and trying to draw a 1, major fixes needed:

---
+++ src/display.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+int main() {
+  short test = 0x0F;
+
+  printf("%x\n", test & 1);
+  for (int bit = 7; bit >= 0; bit--) {
+    short b = (test >> bit) & 1;
+    printf("%d ", b);
+  }
+}
+*/
--- src/main.c
+++ src/main.c
@@ -1,15 +1,50 @@
+#include "../include/memory.h"
 #include "../include/renderer.h"
-#include "../include/timer.h"
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_render.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
+
+  init_memory();
+
+  // test values
+  uint16_t VX = 0;
+  uint16_t VY = 40;
+
+  uint16_t display[64 * 32];
+  int index = VX + 64 * VY;
+
+  memset(display, 0, sizeof(display));
+  for (int i = 0x50; i < 0x55; i++) {
+    short byte = memory[i];
+    short bit;
+    for (int b = 7; b >= 0; b--) {
+      bit = (byte >> b) & 1;
+      if (bit == 1) {
+        display[index] = 1;
+      } else {
+        display[index] = 0;
+      }
+      VX++;
+      index = VX * 64 + VY;
+    }
+    VY++;
+  }
+
+  for (int i = 0; i < 64 * 32; i++) {
+    // printf("%d ", display[i]);
+    if (i % 10 == 0) {
+      // printf("\n");
+    }
+  }
+
   struct SDL_struct sdl_struct;
   sdl_struct.sdl_init = sdl_init;
   sdl_struct.sdl_init(&sdl_struct, 500, 500, "something");
-
-  Timer timer;
-  init_timer(&timer);
-  timer.start(&timer);
 
   while (1) {
     int finished = 0;
@@ -25,8 +60,20 @@ int main(int argc, char *argv[]) {
       break;
     }
 
-    SDL_SetRenderDrawColor(sdl_struct.renderer, 255, 255, 255, 255);
+    // Clear screen
+    SDL_SetRenderDrawColor(sdl_struct.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderClear(sdl_struct.renderer);
+
+    for (int x = 0; x < 64; x++) {
+      for (int y = 0; y < 32; y++) {
+        int index = x + 64 * y;
+        if (display[index] == 1) {
+          const SDL_FRect rect = {x, y, 10, 10};
+          SDL_SetRenderDrawColor(sdl_struct.renderer, 255, 0, 0, 255);
+          SDL_RenderFillRect(sdl_struct.renderer, &rect);
+        }
+      }
+    }
     SDL_RenderPresent(sdl_struct.renderer);
   }
 
